9a3df505d273f117a5c8e02b02c0c9992e3c7a3f
Junnama Noda <junnama@alfasado.jp>
2018-01-21 10:25:26 +0900

    Add plugin Tasks.

--- prototype/tools/worker.php
+++ prototype/tools/worker.php
@@ -18,7 +18,7 @@ if ( file_exists( $pid ) ) {
 touch( $pid );
 require_once( 'lib' . DS . 'Prototype' . DS . 'class.PTPublisher.php' );
 $pub = new PTPublisher;
-$pub->publish_queue( 300000 );
+$pub->publish_queue();
 $ts = time();
 $objects = [];
 $sth = $app->db->model( 'session' )->load_iter( ['expires' => ['<' => $ts ] ] );
@@ -27,12 +27,56 @@ while( $result = $sth->fetch( PDO::FETCH_ASSOC ) ) {
     $objects[] = $obj;
     if ( count( $objects ) >= $bulk_remove_per ) {
         $app->db->model( 'session' )->remove_multi( $objects );
-        unset( $objects );
         $objects = [];
     }
 }
 if (! empty( $objects ) ) {
     $app->db->model( 'session' )->remove_multi( $objects );
-    unset( $objects );
+}
+$tasks = $app->registry['tasks'];
+$event_tasks = [];
+foreach ( $tasks as $key => $regi ) {
+    $priority = isset( $regi['priority'] ) ? (int) $regi['priority'] : 5;
+    $event_tasks[ $priority ] = isset( $event_tasks[ $priority ] )
+                              ? $event_tasks[ $priority ] : [];
+    $regi['id'] = $key;
+    unset( $regi['priority'] );
+    $event_tasks[ $priority ][] = $regi;
+}
+ksort( $event_tasks );
+$lavels = [];
+foreach ( $event_tasks as $tasks ) {
+    foreach ( $tasks as $task ) {
+        $component = $app->component( $task['component'] );
+        $meth = $task['method'];
+        if ( method_exists( $component, $meth ) ) {
+            $frequency = isset( $task['frequency'] ) ? $task['frequency'] : 900;
+            $task_id = $task['id'];
+            $session = $app->db->model( 'session' )->get_by_key(
+                                                ['kind' => 'TK', 'name' => md5( $id ) ] );
+            if ( $session->id ) {
+                $start = $session->start;
+                $time_limit = $start + $frequency;
+                if ( $time_limit > time() ) {
+                    continue;
+                }
+            }
+            $label = $component->translate( $task['label'] );
+            try {
+                $start = time();
+                $component->$meth( $app );
+                $lavels[] = $label;
+                $session->start( $start );
+                $session->expires( $start + $frequency + 3600 );
+                $session->save();
+            } catch ( Exception $e ) {
+                $error = $e->getMessage();
+                $error = $app->translate( "An error occurred in task '%s'. (%s)",
+                                                                    [ $label, $error ] );
+                $log = ['level' => 'error', 'category' => 'task', 'message' => $error ];
+                $app->log( $log );
+            }
+        }
+    }
 }
 unlink( $pid );
