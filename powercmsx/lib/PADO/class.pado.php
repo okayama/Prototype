2fdf9acaeae350a99ac38efc5c1615ffb34c930e
Junnama Noda <junnama@alfasado.jp>
2019-04-11 12:33:20 +0900

    PHP_INT_MAXが2147483647の時に日付が正しく処理されない問題の修正

--- powercmsx/lib/PADO/class.pado.php
+++ powercmsx/lib/PADO/class.pado.php
@@ -2211,7 +2211,7 @@ class PADOBaseModel {
         if ( isset( $pado->methods['db2ts'] ) )
             return $this->_driver->db2ts( $ts );
         $ts = preg_replace( '/[^0-9]/', '', $ts );
-        $ts = (int) $ts;
+        $ts += 0;
         return $ts;
     }
 }
