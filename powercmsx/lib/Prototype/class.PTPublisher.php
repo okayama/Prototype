55bcc38482908bc1e102e7fcf2d6b79961c4a7e7
Alfasado <junnama@alfasado.jp>
2019-06-25 10:33:46 +0900

    ダイナミックでテンプレート変数current_archive_titleをセット

--- powercmsx/lib/Prototype/class.PTPublisher.php
+++ powercmsx/lib/Prototype/class.PTPublisher.php
@@ -151,6 +151,7 @@ class PTPublisher {
                 $ctx->vars['publish_type'] = $mapping->publish_file;
                 $ctx->vars['theme_static'] = $app->theme_static;
                 $ctx->vars['current_archive_type'] = $ctx->stash( 'current_archive_type' );
+                $ctx->vars['current_archive_title'] = $ctx->stash( 'current_archive_title' );
                 $ctx->vars['current_archive_url'] = $url->url;
                 // if ( stripos( $tmpl, 'setvartemplate' ) !== false ) {
                 //     $ctx->compile( $tmpl, false );
